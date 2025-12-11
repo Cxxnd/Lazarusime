39285183b63d3a928bd4fa7b22cc4d7ed172da72
Cxxnd <bwong0227@gmail.com>
2025-12-11 08:40:20 +0700

    middle

--- src/libs/middleware.js
+++ src/libs/middleware.js
@@ -1,17 +1,21 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
-    // Jalan hanya saat mode maintenance diaktifkan
     if (process.env.MAINTENANCE === "1") {
         const { pathname } = request.nextUrl;
 
-        // Jangan redirect untuk file internal dan halaman maintenance
         if (
             !pathname.startsWith("/_next") &&
-            pathname !== "/maintenence"
+            pathname !== "/maintenance" &&
+            !pathname.includes(".")
         ) {
             return NextResponse.redirect(new URL("/maintenance", request.url));
         }
     }
+
     return NextResponse.next();
 }
+
+export const config = {
+    matcher: "/:path*",
+};
